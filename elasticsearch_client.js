70f8ad98eac493e56ab764039d2efed95cc52493
Toufik-SDZ-Souadia <38835412+tawfik-sdz-souadia@users.noreply.github.com>
2020-04-10 09:00:52 -0600

    Update elasticsearch_client.js

--- elasticsearch_client.js
+++ elasticsearch_client.js
@@ -7,13 +7,13 @@ const realm = process.env.CBREALM;
 
 var connection = new autobahn.Connection({url: url, realm: realm});
 
-async function run (name) {
+async function run (name,id) {
   // Let's start by indexing some data
   await client.index({
     index: 'test_dates2',
     // type: '_doc', // uncomment this line if you are using {es} ≤ 6
     body: {
-      id: 1,
+      id: id,
       event: name,
       date: new Date()
     }
@@ -21,12 +21,15 @@ async function run (name) {
 }
 //show information about connection state
 client.ping().then(data => { console.log(data) })
+var id = 0;
+//
 connection.onopen = function (session) {
     console.log("ok");
     function onevent(args) {
           console.log("Got event:", args[0]);
           var name = args[0];
-          run(name).catch(console.log)
+          run(name,id).catch(console.log)
+          id++;
       }
       session.subscribe('com.myapp.hello', onevent);
   };
